80a5cf61e1298eadf553e2c6fb62f390abb19174
neTpyceB <pvman@inbox.ru>
2015-11-28 23:44:42 +0200

    Code refactoring

--- src/neTpyceB/TMCms/Modules/EmailTemplates/Object/EmailTemplate.php
+++ src/neTpyceB/TMCms/Modules/EmailTemplates/Object/EmailTemplate.php
@@ -3,6 +3,12 @@ namespace neTpyceB\TMCms\Modules\EmailTemplates\Object;
 
 use neTpyceB\TMCms\Orm\Entity;
 
+/**
+ * Class EmailTemplate
+ * @package neTpyceB\TMCms\Modules\EmailTemplates\Object
+ *
+ * @method string getContent()
+ */
 class EmailTemplate extends Entity {
     protected $db_table = 'm_email_templates';
     protected $translation_fields = ['content'];
